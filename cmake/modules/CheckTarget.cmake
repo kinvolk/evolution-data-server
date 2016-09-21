03358f788972d122d90246acfb032de316c8a406
Milan Crha <mcrha@redhat.com>
2016-09-21 21:51:50 +0200

    Build camel/tests as part of 'check' target

---
+++ cmake/modules/CheckTarget.cmake
@@ -0,0 +1,20 @@
+# CheckTarget.cmake
+#
+# Defines a custom target 'check', which gathers test programs like 'make check'
+# This is taken from https://cmake.org/Wiki/CMakeEmulateMakeCheck
+#
+# What you do is to call command:
+# add_check_test(_name)
+#   where _name is the name of the test, as defined by add_executable()
+
+include(CTest)
+
+# Disable this not have verbose tests
+set(CMAKE_CTEST_COMMAND ${CMAKE_CTEST_COMMAND} -V)
+
+add_custom_target(check COMMAND ${CMAKE_CTEST_COMMAND})
+
+macro(add_check_test _name)
+	add_test(NAME ${_name} COMMAND ${_name})
+	add_dependencies(check ${_name})
+endmacro(add_check_test)
